708a6f6a77110a3e2781e8520ec24ad0ebcca112
Mateusz Lawczys <ma6tek@gmail.com>
2017-11-23 22:45:48 +0100

    Add script to js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,7 @@
 // scripts.js
 
-//Example comment
-
-// var name = prompt('Enter your name');  - line comment disabling this element of code
-/* var surname = prompt('And your surname !');
-alert('Hello, ' + name + ' ' + surname);
-console.log('Hello, ' + name + surname);  - block comment */
-var phone = prompt('Enter your phone number:');
-alert('You should never do this again, fool! xD');
-console.log('Fool\'s number: ' + phone);
+var a = prompt('Podaj podstawę trójkąta:'),
+    h = prompt('Podaj wysokość trójkąta:'),
+    triangleArea = a*h/2;
+alert('Pole trójkąta wynosi: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
